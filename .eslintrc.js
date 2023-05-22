737c5a53cac43dc2afddd52dbd871d101b6d7987
Matthew Kirkley <matt.kirkley@gmail.com>
2023-05-21 21:50:00 -0400

    package.json upda te

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-param-reassign': 'off',
+    'vue/multi-word-component-names': 'off',
   },
   ignorePatterns: ['**/dist/*.js'],
   overrides: [
